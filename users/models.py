6f38546ef715860b8dd20c710f47d8f3a131de0a
Monirul Jim <monirulislamjim6640@gmail.com>
2025-01-08 22:26:18 +0600

    just make user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
+
+
+class User(AbstractBaseUser):
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    mobile = models.CharField(max_length=20)
+    password = models.CharField(max_length=255)
+    otp = models.CharField(max_length=6, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
