f2cdeb720e353ae6678ca09959e2ca9b577d12af
Monirul Jim <monirulislamjim6640@gmail.com>
2025-01-08 22:44:24 +0600

    make custom user model and usermanager model for handle user

--- users/models.py
+++ users/models.py
@@ -1,6 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from users.models import UserManager
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser should have staff status true')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser should have superuser status true')
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser):
@@ -15,3 +36,7 @@ class User(AbstractBaseUser):
     otp = models.CharField(max_length=6, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    object = UserManager
